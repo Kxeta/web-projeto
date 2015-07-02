f8adfbb6dff4b79f7df0097ada43b069bcdfc544
Phillipe Caixeta Rodrigues <caixeta.phillipe@gmail.com>
2015-07-02 20:13:50 -0300

    Commit Inicial

---
+++ server/app.js
@@ -0,0 +1,44 @@
+
+var app     = express();
+var express = require("express");
+
+// Configuração com o banco de dados:
+var mysql    = require('mysql');
+var pool     =    mysql.createPool({
+    connectionLimit : 100, //importante limitar
+    host     : '54.207.9.240',
+    user     : 'dreamtech',
+    password : '00dreaming00',
+    database : 'web_tp2_caixeta',
+    debug    :  false
+});
+
+function handle_database(req,res) {
+   
+    pool.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          res.json({"code" : 100, "status" : "Erro na conexão com a base de dados"});
+          return;
+        }
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Erro na conexão com a base de dados"});
+              return;    
+        });
+  });
+}
+
+
+
+// configurar qual templating engine usar.
+app.set('view engine', 'hbs');
+
+// define onde estão as views
+app.set('views', 'server/views');
+
+// configurar para servir os arquivos estáticos da pasta "client"
+app.use(express.static('client'));
+
+
+// abrir servidor
+app.listen(3000);
